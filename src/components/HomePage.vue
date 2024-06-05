3f90ae4ac8c2aba5d1908191da34cdeffd8c4cc7
ruchit <ruchit.g@zecdata.com>
2024-06-05 19:42:25 +0530

    done with styling

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,13 +1,59 @@
 <template>
-  <h1>Home page</h1>
-  <div v-for="product in products" :key="product.id">
-    <div class="card" v-on:click="goToProductsPage(product.id)">
-      <img :src="product.thumbnail" />
-      <h2>Brand : {{ product.brand }}</h2>
-      <h4 style="color: slategrey">Category : {{ product.category }}</h4>
-      <h2>Price : ${{ product.price }}</h2>
-    </div>
-  </div>
+  <v-container class="bg-surface-variant">
+    <h1>p r o d u c t s</h1>
+    <v-row no-gutters>
+      <v-col cols="12" sm="8" lg="4" v-for="product in products" :key="product.id">
+        <v-sheet class="ma-2 pa-2">
+          <v-img height="250" :src="product.thumbnail" cover></v-img>
+          <v-card-item>
+            <v-card-title>{{ product.brand }}</v-card-title>
+
+            <v-card-subtitle>
+              <span class="me-1">$ {{ product.price }}</span>
+
+              <v-icon color="error" icon="mdi-fire-circle" size="small"></v-icon>
+            </v-card-subtitle>
+          </v-card-item>
+          <v-card-text>
+            <v-row align="center" class="mx-0">
+              <v-progress-linear
+                bg-color="#92aed9"
+                buffer-color="#6a3e0b"
+                buffer-opacity="1"
+                :buffer-value="product.rating"
+                color="#12512a"
+                height="12"
+                max="9"
+                min="0"
+                :model-value="2"
+                rounded
+              ></v-progress-linear>
+
+              <div class="ms-4 text-h6">{{ product.rating }} / 5</div>
+            </v-row>
+            <br />
+          </v-card-text>
+
+          <v-divider class="mx-4 mb-1"></v-divider>
+
+          <div class="my-4 text-subtitle-1">Category : {{ product.category }}</div>
+
+          <!-- <div>
+            {{ product.description }}
+          </div> -->
+          <v-card-actions>
+            <v-btn
+              color="deep-purple-lighten-2"
+              text="Add To Cart"
+              block
+              border:2
+              @click="goToProductsPage(product.id)"
+            ></v-btn>
+          </v-card-actions>
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
@@ -17,13 +63,9 @@ import { productsStore } from '@/stores/products'
 
 const store = productsStore()
 
-// const router = useRouter()
-
 const products = ref([])
 
 const router = useRouter()
-
-//using pinia
 
 onMounted(async () => {
   const result = await fetch('https://dummyjson.com/products')
--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@ import './assets/main.css'
 
 import { createPinia } from 'pinia'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(createPinia()).use(router).mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives
+})
+
+createApp(App).use(createPinia()).use(router).use(vuetify).mount('#app')
