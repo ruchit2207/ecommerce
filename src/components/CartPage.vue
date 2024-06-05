563d16d0c61f9e65ec1805b3e54ec630ef825f10
ruchit <ruchit.g@zecdata.com>
2024-06-05 18:07:42 +0530

    done with cart page

--- src/components/CartPage.vue
+++ src/components/CartPage.vue
@@ -1,7 +1,35 @@
 <template>
-  <div>Cart</div>
+  <button v-on:click="router.push({ name: 'HomePage' })">Back to Products Page !!</button>
+  <div>
+    <h1>Cart</h1>
+    <div v-if="!store.cart.length">Cart is empty!!</div>
+    <div v-else>
+      <div v-for="item in store.cart" :key="item.id">
+        <div>
+          <img :src="item.thumbnail" />
+          <span>Brand: {{ item.brand }}</span>
+          <span>Category: {{ item.category }}</span>
+          <span>Price: ${{ item.price }}</span>
+          <button @click="removeFromCart(item.id)">Remove</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { productsStore } from '@/stores/products'
+import { useRouter } from 'vue-router'
+
+const store = productsStore()
+
+const router = useRouter()
+
+console.log(store.cart)
+
+const removeFromCart = (id) => {
+  store.removeFromCart(id)
+}
+</script>
 
 <style lang="scss" scoped></style>
